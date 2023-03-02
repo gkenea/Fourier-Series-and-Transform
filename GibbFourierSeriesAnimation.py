f5931c13a78ec9668167769b83740573152f2df0
gkenea <106751225+gkenea@users.noreply.github.com>
2023-03-02 15:12:43 +0300

    Animation of Gibbs Fourier Series

---
+++ GibbFourierSeriesAnimation.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation, rc
+from IPython.display import HTML
+plt.rcParams['figure.figsize'] = [8, 8]
+plt.rcParams.update({'font.size': 18})
+plt.rcParams['animation.html'] = 'jshtml'
+
+
+dx = 0.01
+L = 10
+x = np.arange(0,L+dx,dx)
+n = len(x)
+nquart = int(np.floor(n/4))
+
+f = np.zeros_like(x)
+f[nquart:3*nquart] = 1
+
+A0 = np.sum(f * np.ones_like(x)) * dx * 2 / L
+fFS = A0/2 * np.ones_like(f)
+
+fig,ax = plt.subplots()
+plt.plot(x,f,color='k')
+fFS_plot, = plt.plot([],[],color='r')
+
+all_fFS = np.zeros((len(fFS),101))
+all_fFS[:,0] = fFS
+
+for k in range(1,101):
+    Ak = np.sum(f * np.cos(2*np.pi*k*x/L)) * dx * 2 / L
+    Bk = np.sum(f * np.sin(2*np.pi*k*x/L)) * dx * 2 / L
+    fFS = fFS + Ak*np.cos(2*k*np.pi*x/L) + Bk*np.sin(2*k*np.pi*x/L)
+    all_fFS[:,k] = fFS
+
+def init():
+    ax.set_xlim(x[0],x[-1])
+    ax.set_ylim(-0.2, 1.2)
+    return fFS
+
+def animate(iter):
+    fFS_plot.set_data(x,all_fFS[:,iter])
+    return fFS_plot
+
+anim = animation.FuncAnimation(fig,animate,init_func=init,frames=101,interval=50,blit=False,repeat=False)
+HTML(anim.to_jshtml())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
